324b6151cfc842f12941c85fb25e53fa71ac019e
gabrielcorbot@gmail.com <gabrielcorbot@gmail.com>
2022-06-29 16:47:11 -0500

    Visualización pantalla 2 funcional

--- js/index.js
+++ js/index.js
@@ -11,7 +11,23 @@ function agregarObjetivos() {
             l_objetivos.push(l_inputs[i].value);
         }
     }
+    if(l_objetivos.length !== 0) {
+        paso2();
+    }
     console.log(l_objetivos)
+}
+
+// Paso 2
+function paso2() {
+    let strPaso2 = "";
+    let it = 0;
+    let strInputs = "<input type='text' placeholder='Aspecto problemático'><input type='text' placeholder='Explicación'>";
+    for (o in l_objetivos) {
+        it++;
+        let strTmp = "<p>" + it + ". " + l_objetivos[o] + "</p><br>" + strInputs.repeat(5);
+        strPaso2 = strPaso2.concat(strTmp);
+    }
+    document.getElementById('div-paso-2').innerHTML = strPaso2;
 }
 
 // Cambiar de vista en el HTML
