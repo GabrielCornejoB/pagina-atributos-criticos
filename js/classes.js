d5aa168ae58299b82a890bb2d8f0d2e42f70608a
gabrielcorbot@gmail.com <gabrielcorbot@gmail.com>
2022-06-29 11:17:37 -0500

    init commit

---
+++ js/classes.js
@@ -0,0 +1,44 @@
+function crearObjetivo(descripcionObjetivo) {
+    return {
+        id,                             // Consecutivo: O001, 0213, ...
+        descripcion: descripcionObjetivo,
+        l_problemas
+    };
+}
+
+function crearProblema(descripcionProblema) {
+    return {
+        id,                             // Consecutivo: P001, P213, ...             
+        descripcion: descripcionProblema,
+        esDeDatos,                      // Bool, define si el problema es o no de datos
+        valorTotal,
+        l_datos,
+        l_explicaciones
+    };
+}
+
+function crearDato(descripcionDato) {
+    return {
+        id,                             // Consecutivo: D001, D213, ...  
+        descripcion: descripcionDato,   // Descripción inicial que ingresa el cliente del dato
+        descripcionFinal,               // De tipo 'Diccionario', descripción final que sale de nuestro diccionario de datos
+        valorParticular
+    };
+}
+
+// Aquí esto es una clase diccionario pero realmente debería ser de tipo DatoDiccionario, luego lo consulto
+function crearDiccionario(descripcionDiccionario) {
+    return {
+        id,
+        descripcion: descripcionDiccionario,
+        l_terminos                      
+    };
+}
+
+// Los Terminos se refieren a las palabras que representan un mismo DatoDiccionario
+function crearTermino(descripcionTermino) {
+    return {
+        id,
+        descripcion: descripcionTermino
+    };
+}
