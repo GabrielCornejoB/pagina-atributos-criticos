4a78afd436eb10e90e86c56defb1f2315cebe722
gabrielcorbot@gmail.com <gabrielcorbot@gmail.com>
2022-06-29 12:59:46 -0500

    Botones para cambiar de paso funcionales

---
+++ js/index.js
@@ -0,0 +1,10 @@
+let current_div = document.getElementById("paso-1")
+
+function mostrarContenido(id_div) {
+    let div = document.getElementById(id_div);
+    if(div.hidden !== false) {
+        current_div.hidden = true;
+        div.hidden = false;
+        current_div = div;
+    }
+}
