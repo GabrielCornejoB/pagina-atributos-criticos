6c6f3330d04ddc061fd4658364b1617b41b9f5a6
gabrielcorbot@gmail.com <gabrielcorbot@gmail.com>
2022-07-01 08:49:30 -0500

    Inicio implementación clases

--- js/index.js
+++ js/index.js
@@ -1,20 +1,75 @@
+// Clases
+function crearEmpresa(id, nombreEmpresa, listaObjetivos) {
+    return {
+        id,
+        nombre: nombreEmpresa,
+        listaObjetivos
+    };
+}
+function crearObjetivo(id, descripcionObjetivo, listaProblemas) {
+    return {
+        id,                             // Consecutivo: O001, 0213, ...
+        descripcion: descripcionObjetivo,
+        listaProblemas
+    };
+}
+function crearProblema(id, descripcionProblema, explicacion, esDeDatos, valorTotal, listaDatos) {
+    return {
+        id,                             // Consecutivo: P001, P213, ...             
+        descripcion: descripcionProblema,
+        explicacion,
+        esDeDatos,                      // Bool, define si el problema es o no de datos
+        valorTotal,
+        listaDatos,
+    };
+}
+function crearDato(id, descripcionDato, descripcionFinal, valorParticular) {
+    return {
+        id,                             // Consecutivo: D001, D213, ...  
+        descripcion: descripcionDato,   // Descripción inicial que ingresa el cliente del dato
+        descripcionFinal,               // De tipo 'Diccionario', descripción final que sale de nuestro diccionario de datos
+        valorParticular
+    };
+}
+// Aquí esto es una clase diccionario pero realmente debería ser de tipo DatoDiccionario, luego lo consulto
+function crearDiccionario(id, descripcionDiccionario, listaTerminos) {
+    return {
+        id,
+        descripcion: descripcionDiccionario,
+        listaTerminos                      
+    };
+}
+// Los Terminos se refieren a las palabras que representan un mismo DatoDiccionario
+function crearTermino(id, descripcionTermino) {
+    return {
+        id,
+        descripcion: descripcionTermino
+    };
+}
+
+// Paso a paso
+let empresa = crearEmpresa("E001", "tmp", []);
+let contadorObjetivos = 0
+let contadorProblemas = 0
+let contadorDatos = 0
+
 // Paso 1
 function isEmpty(str) {
     return !str.trim().length;
 }
-let listaObjetivos = []
 function agregarObjetivos() {
-    listaObjetivos = []
+    empresa.listaObjetivos = []
     let listaInputs = document.getElementsByClassName("input-paso-1")
     for (let i=0; i<listaInputs.length; i++) {
         if(!isEmpty(listaInputs[i].value)) {
-            listaObjetivos.push(listaInputs[i].value);
+            // let tmpObjetivo = crearObjetivo()
+            empresa.listaObjetivos.push(listaInputs[i].value);
         }
     }
-    if(listaObjetivos.length !== 0) {
+    if(empresa.listaObjetivos.length !== 0) {
         creacionInputsPaso2();
     }
-    console.log(listaObjetivos)
+    console.log(empresa.listaObjetivos)
     mostrarContenido("paso-2");
 }
 
@@ -23,9 +78,9 @@ function creacionInputsPaso2() {
     let strPaso2 = "";
     let it = 0;
     let strInputs = "<input type='text' placeholder='Aspecto problemático' class='input-paso-2'><input type='text' placeholder='Explicación'>";
-    for (o in listaObjetivos) {
+    for (o in empresa.listaObjetivos) {
         it++;
-        let strTmp = "<p>" + it + ". " + listaObjetivos[o] + "</p><br>" + "<div class='div-paso-2-2'>" + strInputs.repeat(5) + "</div><br><br>";
+        let strTmp = "<p>" + it + ". " + empresa.listaObjetivos[o] + "</p><br>" + "<div class='div-paso-2-2'>" + strInputs.repeat(5) + "</div><br><br>";
         strPaso2 = strPaso2.concat(strTmp);
     }
     // strPaso2 = strPaso2.concat("<button onclick='creacionCheckBoxPaso3()'>Confirmar</button>")
