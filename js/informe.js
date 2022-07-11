c1db85ba51709cb3ecaf5619fc5186aa23fc0e12
gabrielcorbot@gmail.com <gabrielcorbot@gmail.com>
2022-07-11 09:50:29 -0500

    Mejoras visuales informe final y comentarios eliminados

--- js/informe.js
+++ js/informe.js
@@ -7,6 +7,7 @@ document.getElementById("fecha-informe").innerHTML = d.getDate() + "/" + (d.getM
 
 let div_objetivos = document.getElementsByClassName("objetivos");
 let strInforme = "";
+let formatoDinero = Intl.NumberFormat('en-US');
 let it = 1;
 for (o in empresa.listaObjetivos) {
     let obj = empresa.listaObjetivos[o];
@@ -14,40 +15,45 @@ for (o in empresa.listaObjetivos) {
                                         "<h2 class='titulo-objetivo'> Objetivo " + it + "</h2>" +
                                         "<p>" + obj.descripcion + "</p>" + 
                                         "<div class='problemas'>");  
+    let it_p = 1;
     for (p in obj.listaProblemas) {
         let prob = obj.listaProblemas[p];
         strInforme = strInforme.concat("<div class='problema " + prob.id + "'>" + 
-                                            "<h3 class='titulo-problema'>" + prob.descripcion + "</h3>" +
+                                            "<h3 class='titulo-problema'>Aspecto problemático " + it_p + ": " + prob.descripcion + "</h3>" +
                                             "<p>" + prob.explicacion + "</p>" + 
                                             "<div class='es-por-datos'>" + 
-                                                "<p>Es ocasionado por calidad de datos?: " + prob.esDeDatos +"</p>" +
-                                                "<p>" + prob.explicacionEsDeDatos + "</p>" + 
-                                            "</div>");
+                                                "<p>Es ocasionado por calidad de datos?: </p>" +
+                                                "<p>" + prob.esDeDatos + "</p>" + 
+                                            "</div>" + 
+                                            "<p style='padding-left:20px; margin-top:0px'>" + prob.explicacionEsDeDatos + "</p>");
+                                            
         if(prob.esDeDatos === 'Si') {
             strInforme = strInforme.concat("<p>Justificación datos problemáticos: " + prob.explicacionDatos + "</p>" + 
                                             "<div class='datos'>");
+            let it_d = 1;
             for (d in prob.listaDatos) {
-                let dato = prob.listaDatos[d];
+                let dato = prob.listaDatos[d];              
                 strInforme = strInforme.concat("<div class='dato " + dato.id + "'>" + 
-                                                    "<h4 class='titulo-dato'>" + dato.descripcion + "</h4>" +
+                                                    "<h4 class='titulo-dato'>Dato " + it_d + ": " + dato.descripcion + "</h4>" +
                                                     "<div class='div-financiero'>" + 
                                                         "<p>Frecuencia de ocurrencia (mensual/anual):</p>" + 
                                                         "<p>" + dato.frecuenciaMensual + " / "+ (dato.frecuenciaMensual * 12) + "</p>" +
                                                     "</div>" +
                                                     "<div class='div-financiero'>" + 
                                                         "<p>Impacto financiero (mensual/anual):</p>" + 
-                                                        "<p>$" + (dato.valorParticularAnual/12) + " / $" + dato.valorParticularAnual + "</p>" +
+                                                        "<p>$" + formatoDinero.format(dato.valorParticularAnual/12) + " / $" + formatoDinero.format(dato.valorParticularAnual) + "</p>" +
                                                     "</div>" + 
                                                     "<div class='div-financiero'><p>Justificación impacto financiero: " + dato.explicacionValor + "</p></div>" +
                                                 "</div><hr>");
+                it_d++;
             }
             strInforme = strInforme.concat( "</div>");
             strInforme = strInforme.concat("<div class='div-financiero'>" + 
                                                 "<p>Impacto financiero total (mensual/anual):</p>" + 
-                                                "<p>$" + (prob.valorTotalAnual/12) + " / $" + prob.valorTotalAnual + "</p>" +
-                                            "</div>");
+                                                "<p>$" + formatoDinero.format(prob.valorTotalAnual/12) + " / $" + formatoDinero.format(prob.valorTotalAnual) + "</p>" +
+                                            "</div>");         
         } 
-        
+        it_p++;
         strInforme = strInforme.concat("</div>");
     }
     strInforme = strInforme.concat("</div></div><hr>");
