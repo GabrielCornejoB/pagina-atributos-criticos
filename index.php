7a2ee4161341cf78792ba8507615fc3ce7eae567
gabrielcorbot@gmail.com <gabrielcorbot@gmail.com>
2022-07-24 13:10:27 -0500

    Diseño temporal del login

--- index.php
+++ index.php
@@ -5,13 +5,20 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+    <link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
-    <p>Login</p>
-    <form action="php/login.php" method="post">
-        <input type="text" name="username" placeholder="Usuario">
-        <input type="password" name="pwd" placeholder="Constraseña">
-        <button type="submit" name="submitLogin">Ingresar</button>
-    </form>
+    <div class="card">
+        <div class="contenido-final">
+            <div class="div-login">
+                <h1>Login</h1>             
+                <form action="php/login.php" method="post">
+                    <input type="text" name="username" placeholder="Usuario"><br>
+                    <input type="password" name="pwd" placeholder="Constraseña"><br>
+                    <button type="submit" name="submitLogin" class='btn_confirmar'>Ingresar</button>                   
+                </form>
+            </div>           
+        </div>       
+    </div>    
 </body>
 </html>
